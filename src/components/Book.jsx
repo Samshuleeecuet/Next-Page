fdcf8e9ccfaa667a7d2e270ebde786786b01457d
Samshul Islam Shakil <samshuleeeweb@gmail.com>
2023-04-05 21:48:26 +0600

    use inset-0 class for card design

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Book = () => {
+    // const [books,setBooks] = useState([])
+    // useEffect(()=>{
+    //     fetch('https://api.itbook.store/1.0/new')
+    //     .then(res => res.json())
+    //     .then(data=> console.log(data));
+    // },[])
+    const booksData = useLoaderData();
+    const {books,total} = booksData;
+    // console.log(books);
     return (
-        <div>
-            <h1>Book Page</h1>
+        <div className='my-container'>
+            <div className="grid gap-6 mb-8 lg:grid-cols-4 sm:grid-cols-2">             
+            {
+               books.map(book=> 
+               <div className='overflow-hidden relative'>
+                    <img src={book.image} alt=""  className='object-cover w-full h-56 md:h-64 xl:h-80'/>
+
+                    <div className='bg-black bg-opacity-75 text-gray-300 absolute inset-0'>
+                        <p>{book.title}</p><br></br>
+                        <p>{book.subtitle}</p><br></br>
+                        <p>Price: {book.price}</p>
+                    </div>
+               </div>
+               
+                ) 
+            }
+
+            </div>
         </div>
     );
 };
