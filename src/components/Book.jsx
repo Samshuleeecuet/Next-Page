65b0d5639c1973790a19e5441d46cca0da4f3003
Samshul Islam Shakil <samshuleeeweb@gmail.com>
2023-04-05 21:59:49 +0600

    add substring for read more..

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -16,13 +16,13 @@ const Book = () => {
             <div className="grid gap-6 mb-8 lg:grid-cols-4 sm:grid-cols-2">             
             {
                books.map(book=> 
-               <div className='overflow-hidden relative'>
+               <div className='overflow-hidden relative transition duration-200 transform hover:-translate-y-2'>
                     <img src={book.image} alt=""  className='object-cover w-full h-56 md:h-64 xl:h-80'/>
 
-                    <div className='bg-black bg-opacity-75 text-gray-300 absolute inset-0'>
+                    <div className='bg-black bg-opacity-75 text-gray-300 absolute opacity-0 hover:opacity-100 inset-0 transition-opacity px-6 py-4 rounded-xl shadow-lg hover:shadow-2xl flex flex-col'>
                         <p>{book.title}</p><br></br>
-                        <p>{book.subtitle}</p><br></br>
-                        <p>Price: {book.price}</p>
+                        <p>{book.subtitle.substring(0,45)} ...</p><br></br>
+                        <p className='mt-auto'> Price: {book.price}</p>
                     </div>
                </div>
                
